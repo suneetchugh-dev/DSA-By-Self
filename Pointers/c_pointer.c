1aa3c844ec74ce620831137a33a4cdbf3e34e150
suneetchugh-dev <valorise078gramming@gmail.com>
2026-07-29 16:47:43 +0530

    revised pointers in college workshop

---
+++ Pointers/c_pointer.c
@@ -0,0 +1,11 @@
+void main()
+{
+    int *ptr, a=20, b=30;
+    ptr = &a;
+    prinf("%d %d %d" , a,b,ptr); ///address of a
+    prinf("\n%d %d %d" , a,b,*ptr); //dereferencing pointer, value at
+    ptr = &b;
+    printfF("")
+}
+
+
